7c54c322f4d16e0a10d49e184adcf45431a8e1c0
Iestyn Pryce <iestyn.pryce@gmail.com>
2014-01-12 20:05:40 +0800

    Add modified main test run script 	modified:   run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,7 +9,13 @@ progs=( ../c/markov
 	../perl/markov.pl
 	../python/markov.py )
 
+alice="../text/gutenberg_alice_in_wonderland.txt"
+
 for p in "${progs[@]}"; do
 	echo "Testing $p"
 	bash test_edge_cases.sh "$p"
+	$p < "$alice" > "alice.out"
+	awk -f test_word_groups.awk "$alice" "alice.out"
+	rm "alice.out"
+	bash test_statistics.sh "$p"
 done
