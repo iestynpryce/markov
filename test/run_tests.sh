2abfdcb356942a3279eeb8294de97261b75f3cdc
Iestyn Pryce <iestyn.pryce@gmail.com>
2014-01-13 22:29:47 +0800

    Split out common code from run_tests.sh into test_common.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 # Run all tests against all programs
 
-progs=( ../c/markov 
-        ../cpp/markov 
-        'java -cp ../java/ Markov'
-	../go/markov
-        ../awk/markov.awk
-	../perl/markov.pl
-	../python/markov.py )
-
-alice="../text/gutenberg_alice_in_wonderland.txt"
+source test_common.sh
 
 for p in "${progs[@]}"; do
 	echo "Testing $p"
---
+++ test/test_common.sh
@@ -0,0 +1,10 @@
+progs=( ../c/markov 
+        ../cpp/markov 
+        'java -cp ../java/ Markov'
+        ../go/markov
+        ../awk/markov.awk
+        ../perl/markov.pl
+        ../python/markov.py )
+
+alice="../text/gutenberg_alice_in_wonderland.txt"
+
